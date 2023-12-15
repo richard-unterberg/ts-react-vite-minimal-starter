eca16ed1db877dceef15a591f8ce110b1df3c45b
Richard Unterberg <mail@richardunterberg.de>
2023-12-15 23:50:44 +0100

    🎨 add memo to lazy icon render output

--- src/lib/icons/LazyIcon.tsx
+++ src/lib/icons/LazyIcon.tsx
@@ -2,13 +2,14 @@ import { LucideProps } from 'lucide-react'
 
 import { ICON_ID } from '@/lib/icons/iconID'
 import APP_ICON from '@/lib/icons/iconMap'
+import { useMemo } from 'react'
 
 interface LazyIconIconProps extends LucideProps {
   icon: ICON_ID
 }
 
 const LazyIcon = ({ icon, ...props }: LazyIconIconProps) => {
-  const AppIconComponent = APP_ICON[icon].component ?? null
+  const AppIconComponent = useMemo(() => APP_ICON[icon].component ?? null, [icon])
 
   if (AppIconComponent) {
     return <AppIconComponent {...props} />
